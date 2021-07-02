8a64395646b74683f11c81155f7810a5256a0bc7
Susan Holland <segh@fastmail.com>
2021-07-01 20:04:27 -0400

    Add from field to send emails from alias

--- getFeedback.js
+++ getFeedback.js
@@ -1,11 +1,9 @@
 function getFeedback() {
-    const myName = "Susan Holland"
-    const htmlMessage = `<p>Thank you!<br>${myName}`
-  
-    MailApp.sendEmail({
-      to: "centraltutorsupport@bootcampspot.com",
-      subject: "Please send me the student feedback from my tutorial sessions",
-      htmlBody: htmlMessage
-    });
-  }
-  
+  const myName = "Susan Holland"
+  const htmlMessage = `<p>Thank you!<br>${myName}`
+
+  GmailApp.sendEmail("centraltutorsupport@bootcampspot.com", "Please send me the student feedback from my tutorial sessions", htmlMessage, {
+    from: "sholland2@bootcampspot.com",
+    htmlBody: htmlMessage
+  });
+}
--- makeFixedSessionReminderDrafts.js
+++ makeFixedSessionReminderDrafts.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function makeFixedSessionReminderDrafts() {
   const students = ContactsApp.getContactGroup('Coding Students W/ Fixed Schedules').getContacts();
   console.log(students)
 
@@ -11,7 +11,6 @@ function myFunction() {
     let day = "";
     let time = "";
 
-    // Set values of custom fields based on label
     for (let i = 0; i < fields.length; i++) {
       let fieldValue = fields[i].getValue();
       let fieldLabel = fields[i].getLabel();
@@ -48,6 +47,6 @@ function myFunction() {
     console.log(nextSession)
     const htmlMessage = `<p>Hi ${name}!<br><br>Thank you for scheduling your session with me. I am looking forward to our session on ${day}, ${nextSession} at ${time} ${timeZone} Time.<br><br>If something comes up and the scheduled time will not work, <strong>let me know a minimum of 6 hours before the appointment time</strong> and we’ll figure something out.<br><br>This session will take place here: ${zoomLink}<br><br>(If you have not used zoom before please join the meeting at least 15 minutes early because it may have you download and install some software.)<br><br>Again, all I need from you:<ul><li>Be on Tutors & Students Slack 5 minutes before your time slot.</li><li>Make sure your computer/mic/internet connection are working.</li><li>Make sure your workspace is quiet and free from interruptions.</li><li>At the end of the session, I will provide you with a link to a 2 minute evaluation form that you are required to complete.</li></ul><br>Slack or email me with any questions.  I’m looking forward to our meeting!<br><br><strong>Please Reply All to this email so that I know you have seen it.</strong><br><br><strong>(CC Central Support on all tutor email by always using REPLY ALL).</strong><br><br>Sincerely,<br><br>Susan`
 
-    GmailApp.createDraft(email, `Coding Boot Camp - Tutorial Confirmation - ${day}, ${nextSession} at ${time} ${timeZone} Time`, htmlMessage, { cc: 'centraltutorsupport@bootcampspot.com ', htmlBody: htmlMessage })
+    GmailApp.createDraft(email, `Coding Boot Camp - Tutorial Confirmation - ${day}, ${nextSession} at ${time} ${timeZone} Time`, htmlMessage, { from: "sholland2@bootcampspot.com", cc: 'centraltutorsupport@bootcampspot.com ', htmlBody: htmlMessage })
   }
 }
